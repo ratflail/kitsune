b99e023f8be7bb7f6cdc0efe435bac9dabba2358
Tulio Dias <tuliodias@gmail.com>
2025-05-05 18:00:22 -0300

    Remove node_modules from tracking

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog.html'),
+      },
+    },
+  },
+});
